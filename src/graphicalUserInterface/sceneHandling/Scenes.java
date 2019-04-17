d58e29800c79412c746d74639302cf246d6de13f
Unknown <sam@malpass.me.uk>
2019-04-17 14:49:21 +0100

    Minor Patch

--- src/graphicalUserInterface/sceneHandling/Scenes.java
+++ src/graphicalUserInterface/sceneHandling/Scenes.java
@@ -4,9 +4,12 @@
  * @since 0.0.0.d
  */
 package graphicalUserInterface.sceneHandling;
+import graphicalUserInterface.sceneHandling.controllers.BeginningController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import programFunctions.ProgramFunctions;
+
 public class Scenes {
     /**
      * dataCache holds all scene related data
